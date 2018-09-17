b202e75e94b379c43898d3fb1cdb6c965fdae1a2
Tiago Ulisses Montini <tmontini@gmail.com>
2018-09-16 21:55:09 -0300

    Primeiro commit

---
+++ src/main/java/com/example/algamoney/api/event/listener/RecursoCriadoListener.java
@@ -0,0 +1,34 @@
+package com.example.algamoney.api.event.listener;
+
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.algamoney.api.event.RecursoCriadoEvent;
+
+@Component
+public class RecursoCriadoListener implements ApplicationListener<RecursoCriadoEvent>{
+
+	@Override
+	public void onApplicationEvent(RecursoCriadoEvent recursoCriado) {
+		HttpServletResponse response  = recursoCriado.getResponse();
+		Long codigo = recursoCriado.getCodigo();
+		
+		adicionarHeaderLocation(response, codigo);
+		
+	}
+
+	private void adicionarHeaderLocation(HttpServletResponse response, Long codigo) {
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequestUri()
+				.path("/{codigo}")
+				.buildAndExpand(codigo)
+				.toUri();
+		response.addHeader("Location", uri.toASCIIString());
+	}
+
+}
---
+++ src/main/resources/db/migration/V02__criar_tabela_pessoa.sql
@@ -0,0 +1,17 @@
+create table pessoa (
+	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	nome varchar(70) not null,
+	logradouro varchar(100),
+	numero varchar(10),
+	complemento varchar(100),
+	bairro varchar(100),
+	cep varchar(10),
+	cidade varchar(100),
+	estado varchar(50),
+	ativo boolean not null
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+insert into pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Tiago Montini', 'Rua Pará', '858', '', 'Vila Mioto', '15.600-000', 'Fernandópolis', 'São Paulo', true);
+insert into pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Renato de Oliveira', 'Avenida Brasil', '1363', 'Ap 12', 'Centro', '15.659-256', 'São Paulo', 'São Paulo', true);
+insert into pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Felicio Arnaldo da Silva', 'Rua 3', '2928', '', 'Jardim Santista', '15.000-000', 'Fernandópolis', 'São Paulo', true);
+insert into pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Fernando da Costa', 'Rua Paraiba', '342', 'Cohab 3', 'Cohab 3', '15.099-033', 'Fernandópolis', 'São Paulo', true);
