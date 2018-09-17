9ad5d260e8f680aa0507ac8d43755afa48c926c4
Tiago Ulisses Montini <tmontini@gmail.com>
2018-09-16 23:03:08 -0300

    Adicionado regra para não salvar pessoa inativa ao lancamento

--- src/main/java/com/example/algamoney/api/model/Pessoa.java
+++ src/main/java/com/example/algamoney/api/model/Pessoa.java
@@ -6,7 +6,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "pessoa")
@@ -56,6 +59,12 @@ public class Pessoa {
 
 	public void setAtivo(boolean ativo) {
 		this.ativo = ativo;
+	}
+	
+	@JsonIgnore
+	@Transient
+	public boolean isInativo() {
+		return !this.ativo;
 	}
 
 	@Override
