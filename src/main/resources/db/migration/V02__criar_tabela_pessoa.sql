85af63fa1aaa61fe8538d0cc21f20cb0730c5bea
Tiago Ulisses Montini <tmontini@gmail.com>
2018-09-30 09:21:46 -0300

    corrigindo os insert para o flyaway

--- src/main/resources/db/migration/V02__criar_tabela_pessoa.sql
+++ src/main/resources/db/migration/V02__criar_tabela_pessoa.sql
@@ -15,3 +15,4 @@ insert into pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, 
 insert into pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Renato de Oliveira', 'Avenida Brasil', '1363', 'Ap 12', 'Centro', '15.659-256', 'São Paulo', 'São Paulo', true);
 insert into pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Felicio Arnaldo da Silva', 'Rua 3', '2928', '', 'Jardim Santista', '15.000-000', 'Fernandópolis', 'São Paulo', true);
 insert into pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Fernando da Costa', 'Rua Paraiba', '342', 'Cohab 3', 'Cohab 3', '15.099-033', 'Fernandópolis', 'São Paulo', true);
+insert into pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Rafael Bortolo', 'Rua Paraiba', '342', 'Cohab 3', 'Cohab 3', '15.099-033', 'Fernandópolis', 'São Paulo', true);
